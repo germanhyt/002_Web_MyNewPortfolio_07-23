bd823d0f88e9b544b3115584585897693414c08b
Dacounter95 <146789960+germanhytsystem@users.noreply.github.com>
2024-03-05 18:45:37 -0500

    fix: Change image to AppBanner completed

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -1,82 +1,106 @@
-import useThemeSwitcher from '../../hooks/useThemeSwitcher';
-import { FiArrowDownCircle } from 'react-icons/fi';
-import developerLight from '../../images/developer.svg';
-import developerDark from '../../images/developer-dark.svg';
-import { motion } from 'framer-motion';
+import useThemeSwitcher from "../../hooks/useThemeSwitcher";
+import { FiArrowDownCircle } from "react-icons/fi";
+import developerLight from "../../images/developer.svg";
+import developerDark from "../../images/developer-dark.svg";
+import { motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
 const AppBanner = () => {
-	const [activeTheme] = useThemeSwitcher();
+  // Hooks
+  const [activeTheme, setTheme] = useThemeSwitcher();
+  let [themeCurrent, setThemeCurrent] = useState("dark");
+  const imageRef = useRef();
 
-	return (
-		<motion.section
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			transition={{ ease: 'easeInOut', duration: 0.9, delay: 0.2 }}
-			className="flex flex-col sm:justify-between items-center sm:flex-row mt-12 md:mt-2"
-		>
-			<div className="w-full md:w-1/3 text-left">
-				<motion.h1
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					transition={{
-						ease: 'easeInOut',
-						duration: 0.9,
-						delay: 0.1,
-					}}
-					className="font-general-semibold text-2xl lg:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light uppercase"
-				>
-					Hola, Soy Germán Huaytalla
-				</motion.h1>
-				<motion.p
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					transition={{
-						ease: 'easeInOut',
-						duration: 0.9,
-						delay: 0.2,
-					}}
-					className="font-general-medium mt-4 text-lg md:text-xl lg:text-2xl xl:text-3xl text-center sm:text-left leading-normal text-gray-500 dark:text-gray-200"
-				>
-					Estudiante de Ingeniería de Sistemas
-				</motion.p>
-				<motion.div
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					transition={{
-						ease: 'easeInOut',
-						duration: 0.9,
-						delay: 0.3,
-					}}
-					className="flex justify-center sm:block"
-				>
-					<a
-						download="CV_Germán_Huaytalla.pdf"
-						href="/files/CV_Germán_Huaytalla.pdf"
-						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
-						aria-label="Download Resume"
-					>
-						<FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiArrowDownCircle>
-						<span className="text-sm sm:text-lg font-general-medium duration-100">
-							Descargar CV
-						</span>
-					</a>
-				</motion.div>
-			</div>
-			<motion.div
-				initial={{ opacity: 0, y: -180 }}
-				animate={{ opacity: 1, y: 0 }}
-				transition={{ ease: 'easeInOut', duration: 0.9, delay: 0.2 }}
-				className="w-full sm:w-2/3 text-right float-right mt-8 sm:mt-0"
-			>
-				<img
-					src={
-						activeTheme === 'dark' ? developerLight : developerDark
-					}
-					alt="Developer"
-				/>
-			</motion.div>
-		</motion.section>
-	);
+  // Methods
+  setInterval(() => {
+    setTheme(localStorage.getItem("theme"));
+
+    if (activeTheme !== localStorage.getItem("theme")) {
+      console.log("No son equals");
+    } else {
+      setThemeCurrent(localStorage.getItem("theme"));
+    }
+
+    // console.log("themeHere: ", themeCurrent);
+  }, 500);
+
+  useEffect(() => {
+    if (themeCurrent === "dark") {
+      imageRef.current.src = developerDark;
+    } else {
+      imageRef.current.src = developerLight;
+    }
+  }, [themeCurrent]);
+
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }}
+      className="flex flex-col sm:justify-between items-center sm:flex-row mt-12 md:mt-2"
+    >
+      <div className="w-full md:w-1/3 text-left">
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            ease: "easeInOut",
+            duration: 0.9,
+            delay: 0.1,
+          }}
+          className="font-general-semibold text-2xl lg:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light uppercase"
+        >
+          Hola, Soy Germán Huaytalla
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            ease: "easeInOut",
+            duration: 0.9,
+            delay: 0.2,
+          }}
+          className="font-general-medium mt-4 text-lg md:text-xl lg:text-2xl xl:text-3xl text-center sm:text-left leading-normal text-gray-500 dark:text-gray-200"
+        >
+          Estudiante de Ingeniería de Sistemas
+        </motion.p>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            ease: "easeInOut",
+            duration: 0.9,
+            delay: 0.3,
+          }}
+          className="flex justify-center sm:block"
+        >
+          <a
+            download="CV_Germán_Huaytalla.pdf"
+            href="/files/CV_Germán_Huaytalla.pdf"
+            className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
+            aria-label="Download Resume"
+          >
+            <FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiArrowDownCircle>
+            <span className="text-sm sm:text-lg font-general-medium duration-100">
+              Descargar CV
+            </span>
+          </a>
+        </motion.div>
+      </div>
+      <motion.div
+        initial={{ opacity: 0, y: -180 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }}
+        className="w-full sm:w-2/3 text-right float-right mt-8 sm:mt-0"
+      >
+        {/* <img
+          src={activeTheme === "dark" ? developerLight : developerDark}
+          alt="Developer"
+        /> */}
+        <img ref={imageRef} alt="Developer" />
+      </motion.div>
+    </motion.section>
+  );
 };
 
 export default AppBanner;
